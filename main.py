bd26743afe83b4101761a674997f976fcf9a7f26
David Brown <david@fairly-brilliant.com>
2020-03-15 10:30:32 -0700

    Added minor CSS styling

--- main.py
+++ main.py
@@ -7,15 +7,26 @@ class Emailkitties(object):
     @cherrypy.expose
     def index(self):
         return """<html>
-          <head></head>
+          <head>
+              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+              <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          </head>
           <body>
           <p>
-          Enter your email if you're tired of COVID-19 emails in your inbox.
+          <h2>Tired of your inbox being nothing but COVID-19 emails?</h2>
+          <p>Us too. Bring a little happiness to your email :)
           <p>
-            <form method="get" action="confirmation">
-              <input type="text" value="Enter Your Email" name="email" />
-              <button type="submit">Click For Kitties!</button>
+          <form method="get" action="confirmation">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else. All the code for this project can be found at https://github.com/devwithdave/kittyemailer</small>
+            <button type="submit" class="btn btn-primary">Click for Kitties!</button>
+          </div>
             </form>
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
           </body>
         </html>"""
 
@@ -23,7 +34,20 @@ class Emailkitties(object):
     def confirmation(self, email):
         password = os.environ.get('GMAIL')
         send_email(server_login('kittyemailer@gmail.com',password), email)
-        return f"Kitties are on their way to {email}!"
+        return f"""<html>
+          <head>
+              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+              <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          </head>
+          <body>
+            <h1>Kitties are on their way to {email}!<h1>
+            <div style="width:50%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/S3KIhtDGjLKWbnwtrQ" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/dog-kitten-fuzzfeed-S3KIhtDGjLKWbnwtrQ">via GIPHY</a></p>
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+          </body>
+        </html>"""  
+        
 
 
 if __name__ == '__main__':
